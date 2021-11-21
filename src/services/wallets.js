7d208e2252924a93f9e6f8a3e9d053fa287bce29
DamianGanopolsky <damian98dgg@gmail.com>
2021-11-21 15:55:23 -0300

    initial files

---
+++ src/services/wallets.js
@@ -0,0 +1,52 @@
+const ethers = require("ethers");
+const accounts = [];
+
+const getDeployerWallet = ({ config }) => () => {
+  const provider = new ethers.providers.InfuraProvider(config.network, config.infuraApiKey);
+  const wallet = ethers.Wallet.fromMnemonic(config.deployerMnemonic).connect(provider);
+  console.log("Deployer wallet" + wallet.address);
+  return wallet;
+};
+/* Crea una wallet nueva con un proveedor (en este caso es infura), nos conectamos a la blockchain kovan a traves
+de infura
+ */
+const createWallet = () => async () => {
+  const provider = new ethers.providers.InfuraProvider("kovan", process.env.INFURA_API_KEY);
+  // This may break in some environments, keep an eye on it
+  /*Se crea una clave publica y privada dentro de la misma maquina SE GUARDA EN TEXTO PLANO. No esta mal
+  * si persistimos las claves en una BDD(mongo o Postrgres)*/
+  const wallet = ethers.Wallet.createRandom().connect(provider);
+  accounts.push({
+    address: wallet.address,
+    privateKey: wallet.privateKey,
+  });
+  const result = {
+    id: accounts.length,
+    address: wallet.address,
+    privateKey: wallet.privateKey,
+  };
+  return result;
+};
+
+const getWalletsData = () => () => {
+  return accounts;
+};
+
+/* Aca estoy obteniendo los datos de la wallet */
+const getWalletData = () => index => {
+  return accounts[index - 1];
+};
+/* Esto es un objeto de la wallet, me encapsula varias cosas */
+const getWallet = ({}) => index => {
+  const provider = new ethers.providers.InfuraProvider("kovan", process.env.INFURA_API_KEY);
+
+  return new ethers.Wallet(accounts[index - 1].privateKey, provider);
+};
+
+module.exports = ({ config }) => ({
+  createWallet: createWallet({ config }),
+  getDeployerWallet: getDeployerWallet({ config }),
+  getWalletsData: getWalletsData({ config }),
+  getWalletData: getWalletData({ config }),
+  getWallet: getWallet({ config }),
+});
