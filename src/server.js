d554fda31d676ea10f9cbb14e4182f1c0cf49fdb
Fernando <ffabbiano@fi.uba.ar>
2021-11-28 02:21:29 -0300

    fix!: make server listen in 0.0.0.0

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ routes.forEach(route => fastify.route(route({ config, services })));
 const start = async () => {
   try {
     /* Escucha requests en el puerto 3000 */
-    await fastify.listen(process.env.PORT || 3000);
+    await fastify.listen(process.env.PORT || 3000, '0.0.0.0');
     fastify.log.info(`server listening on ${fastify.server.address().port}`);
   } catch (err) {
     fastify.log.error(err);
