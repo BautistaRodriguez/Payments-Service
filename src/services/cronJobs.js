926f7d929f84388af847963839696ae78e1972d7
Fernando <ffabbiano@fi.uba.ar>
2021-12-01 17:35:47 -0300

    feat: update send payments job

--- src/services/cronJobs.js
+++ src/services/cronJobs.js
@@ -15,23 +15,27 @@ const sendProfessorsPayemnts = () => {
   // Get wallet service
   deployerWaller = services.walletService.getDeployerWallet(config)
 
-  // Get SC founds
-  scTotalFounds = 2
+  cron.schedule('* * * * *', function() {
+    logInfo("Running job to send payments to proffesor")
 
-  getCollaborators(function(collaborators) {
-    logInfo("Got " + collaborators.data.length + " collaborators")
+    getCollaborators(function(collaborators) {
+      logInfo("Got " + collaborators.data.length + " collaborators")
 
-    paymentForEach = collaborators.data.length / scTotalFounds
-    logInfo("Each collaborator will receive " + paymentForEach + " ether(s)")
+      // TODO Get SC founds
+      scTotalFounds = 0.01
 
-    // Parse response json
-    collaborators = collaborators.data
+      paymentForEach = collaborators.data.length / scTotalFounds
+      logInfo("Each collaborator will receive " + paymentForEach + " ether(s)")
 
-    cron.schedule('* * * * *', function() {
-      logInfo("Running job to send payments to proffesor")
-      //services.contractInteraction.sendMoneyToWallet("0x1e725fbe268df79977c60565eac9066f2f5be095", '0.001', deployerWaller)
-    });
-  })
+      // Parse response json
+      collaborators = collaborators.data
 
+      // TODO
+      for(var collaborator in collaborators){
+        logInfo("Sending " + paymentForEach + " to user id: " + collaborator['id'])
+        wallet = findWallet(collaborator['id'])
+        //services.contractInteraction.sendMoneyToWallet(wallet, paymentForEach, deployerWaller)
+      }
+    })
+  });
 }
-
